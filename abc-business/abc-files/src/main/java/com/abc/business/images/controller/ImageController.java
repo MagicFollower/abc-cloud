91de97d8cd6c950b262d8eafbfaab7c0802643fe
trivis <webturing256@163.com>
2023-02-10 22:57:41 +0800

    新增多图片上传接口

---
+++ abc-business/abc-files/src/main/java/com/abc/business/images/controller/ImageController.java
@@ -0,0 +1,52 @@
+package com.abc.business.images.controller;
+
+import com.abc.business.images.domain.dto.ImageUploadDTO;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.util.Objects;
+
+/**
+ * @author trivis
+ */
+@RestController
+@RequestMapping("/images")
+public class ImageController {
+
+    @PostMapping("/upload_s")
+    public void singleImageImport(@RequestBody ImageUploadDTO imageUploadInfo) {
+
+        try (BufferedOutputStream bo = new BufferedOutputStream(new FileOutputStream("backup-" + imageUploadInfo
+                .getFilename()))) {
+            bo.write(Base64Utils.decodeFromString(imageUploadInfo.getImage()));
+            bo.flush();
+        } catch (IOException e) {
+            // todo log
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * 根据表单属性解析出Multipart，可以使用@RequestPart、也可以使用@RequestParam
+     *
+     * @param fileArray 多文件
+     */
+    @PostMapping("/upload")
+    public void multiImageImport(@RequestPart("files") MultipartFile[] fileArray) {
+
+        for (MultipartFile multipartFile : fileArray) {
+            String filename = Objects.requireNonNull(multipartFile.getOriginalFilename());
+            try (
+                    OutputStream os = new FileOutputStream(filename);
+                    InputStream is = multipartFile.getInputStream();
+            ) {
+                is.transferTo(os);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
