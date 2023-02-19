5fbf9dfe24394d1d74e6e7dd7d89e25e55cb0efa
trivis <webturing256@163.com>
2023-02-20 00:10:42 +0800

    新增分页操作文档

--- abc-common/abc-common-core/src/main/java/com/abc/common/core/web/page/TableDataInfo.java
+++ abc-common/abc-common-core/src/main/java/com/abc/common/core/web/page/TableDataInfo.java
@@ -12,17 +12,17 @@ public class TableDataInfo implements Serializable
 {
     private static final long serialVersionUID = 1L;
 
-    /** 总记录数 */
-    private long total;
+    /** 消息状态码 */
+    private int code;
 
     /** 列表数据 */
     private List<?> rows;
 
-    /** 消息状态码 */
-    private int code;
-
     /** 消息内容 */
     private String msg;
+
+    /** 总记录数 */
+    private long total;
 
     /**
      * 表格数据对象
