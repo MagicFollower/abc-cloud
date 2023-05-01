dca449ff6e658f798507e981958fe645350311ee
YuXiang <webturing256@163.com>
2023-05-01 16:52:30 +0800

    新增abc-common-redis模块： 1.封装RedisService； 2.使用Fastjson2作为正反序列化器。

---
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-redis/src/main/java/com/abc/system/common/redis/config/RedisConfig.java
@@ -0,0 +1,43 @@
+package com.abc.system.common.redis.config;
+
+import com.abc.system.common.redis.serializer.FastJson2JsonRedisSerializer;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * RedisConfig
+ *
+ * @Description RedisConfig
+ * @Author Trivis
+ * @Date 2023/5/1 15:40
+ * @Version 1.0
+ */
+@EnableCaching
+@AutoConfigureBefore(RedisAutoConfiguration.class)
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    @SuppressWarnings(value = {"unchecked", "rawtypes"})
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        FastJson2JsonRedisSerializer serializer = new FastJson2JsonRedisSerializer(Object.class);
+
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+
+        // Hash的key也采用StringRedisSerializer的序列化方式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
---
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-redis/src/main/java/com/abc/system/common/redis/serializer/FastJson2JsonRedisSerializer.java
@@ -0,0 +1,49 @@
+package com.abc.system.common.redis.serializer;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONReader;
+import com.alibaba.fastjson2.JSONWriter;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * FastJson2JsonRedisSerializer
+ *
+ * @Description FastJson2JsonRedisSerializer 使用fastjson2作为redis的序列化器
+ * @Author Trivis
+ * @Date 2023/5/1 15:59
+ * @Version 1.0
+ */
+public class FastJson2JsonRedisSerializer<T> implements RedisSerializer<T> {
+    public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+
+    private final Class<T> clazz;
+
+
+    public FastJson2JsonRedisSerializer(Class<T> clazz) {
+        super();
+        this.clazz = clazz;
+    }
+
+    @Override
+    public byte[] serialize(T t) throws SerializationException {
+        if (t == null) {
+            return new byte[0];
+        }
+        return JSONObject.toJSONString(t, JSONWriter.Feature.WriteClassName).getBytes(DEFAULT_CHARSET);
+    }
+
+    @Override
+    public T deserialize(byte[] bytes) throws SerializationException {
+        if (bytes == null || bytes.length <= 0) {
+            return null;
+        }
+        String str = new String(bytes, DEFAULT_CHARSET);
+
+        return JSONObject.parseObject(str, clazz, JSONReader.Feature.SupportAutoType);
+    }
+}
+
