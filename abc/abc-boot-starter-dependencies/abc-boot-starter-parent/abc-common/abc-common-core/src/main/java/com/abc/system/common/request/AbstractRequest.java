870e4770f23f46f4a28c9b566465aef6aa314d42
YuXiang <webturing256@163.com>
2023-05-04 19:13:21 +0800

    更新类注释文档

--- abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/request/AbstractRequest.java
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/request/AbstractRequest.java
@@ -1,14 +1,29 @@
 package com.abc.system.common.request;
 
+import com.abc.system.common.exception.BizException;
 import lombok.Data;
 
 import java.io.Serializable;
 
 /**
  * 系统内部Service层请求基类
- * 1.提供简单的分页参数（pageNum + pageSize）
+ * 1.Controller层请继承并抽象出AbstractBaseRequest,你可以把一些模板参数封装在内部，例如公司编号）
  */
 @Data
 public abstract class AbstractRequest implements Serializable {
+    /**
+     * 自定义分页实体（与Mybatis无关）
+     * 1. pageNum:  int
+     * 2. pageSize: int
+     */
     protected PageInfo pageInfo;
+
+    /**
+     * 查询请求参数校验，这是一个tip，它会提醒你要在每一个服务中对参数进行校验
+     * 1. 你可以为CRUD+Stop都设置单独的校验方法，和这里一样；
+     * 2. 这个方法约定用于查询参数的请求校验。
+     *
+     * @throws BizException 校验失败时，抛出BizException
+     */
+    abstract void requestCheck() throws BizException;
 }
--- abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/request/PageInfo.java
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/request/PageInfo.java
@@ -6,13 +6,13 @@ import lombok.NoArgsConstructor;
 
 /**
  * 系统内部分页实体（供AbstractRequest使用）
- * 1.pageNum:Integer  页数
- * 2.pageSize:Integer 页大小
+ * 1.pageNum:  int  页数
+ * 2.pageSize: int 页大小
  */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PageInfo {
-    private Integer pageNum = 1;
-    private Integer pageSize = 10;
+    private int pageNum = 1;
+    private int pageSize = 10;
 }
