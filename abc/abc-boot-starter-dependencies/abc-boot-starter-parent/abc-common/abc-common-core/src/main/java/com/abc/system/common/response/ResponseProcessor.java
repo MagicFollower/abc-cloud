33d9632db3c54ec5abde53f5928a206977c691d3
YuXiang <webturing256@163.com>
2023-05-04 20:21:35 +0800

    精简ResponseProcessor，统一Controller层接口响应方式🔓

--- abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/response/ResponseProcessor.java
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/response/ResponseProcessor.java
@@ -8,18 +8,15 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.Serializable;
 
 /**
- * 响应处理器工具，Controller层使用，提供统一的API，用于快速生成ResponseData。
+ * 响应处理器工具，Controller层使用，提供2个统一的API，用于快速生成ResponseData。
  *
  * @param <T> 响应体中result数据类型（基本实体、实体集合）
  * @Description 响应处理器工具，Controller层使用，提供统一的API，用于快速生成ResponseData。<br>
  * → 成功：<br>
  * → new ResponseData泛型().setData(response.getResult());<br>
- * → new ResponseData泛型().setData(response.getResult(), "操作成功");<br>
  * → 异常：<br>
- * → new ResponseData泛型().setErrorMessage(be.getMessage);<br>
- * → new ResponseData泛型().setErrorMessage(be.getErrorCode, be.getMessage);<br>
- * → new ResponseData泛型().setErrorMessage(SystemReturnCodeConstants.SYSTEM_ERROR.getMessage());<br>
- * → new ResponseData泛型().setErrorMessage(SystemReturnCodeConstants.SYSTEM_ERROR.getCode(),SystemReturnCodeConstants.SYSTEM_ERROR.getMessage());<br>
+ * → new ResponseData空泛型().setErrorMessage(be.getErrorCode, be.getMessage);<br>
+ * → new ResponseData空泛型().setErrorMessage(SystemReturnCodeConstants.SYSTEM_ERROR.getCode(),SystemReturnCodeConstants.SYSTEM_ERROR.getMessage());<br>
  * ⚠️提示：ResponseData在此处忽略泛型<br>
  * 1.setErrorMsg在提供内部错误编号和信息时，将会封装为ResponseErrorData至result中，使controller层代码泛型统一为T<br>
  */
@@ -27,59 +24,37 @@ public class ResponseProcessor<T> {
     private final ResponseData responseData = new ResponseData();
 
     public ResponseProcessor() {
-        this.responseData.setSuccess(true);
-        this.responseData.setMessage("success");
-        this.responseData.setCode(200);
     }
 
+    /**
+     * 🔓统一成功响应消息
+     *
+     * @param t t
+     * @return ResponseData
+     */
     public ResponseData<T> setData(T t) {
-        this.responseData.setResult(t);
         this.responseData.setSuccess(true);
         this.responseData.setCode(200);
-        return this.responseData;
-    }
-
-    public ResponseData<T> setData(T t, String msg) {
+        this.responseData.setMessage("操作成功");
         this.responseData.setResult(t);
-        this.responseData.setSuccess(true);
-        this.responseData.setMessage(msg);
-        this.responseData.setCode(200);
         return this.responseData;
     }
 
-    public ResponseData<Object> setData(int code, String errorCode, String msg) {
-        this.responseData.setSuccess(true);
-        this.responseData.setMessage(msg);
-        this.responseData.setCode(code);
-        if (StringUtils.isNotEmpty(errorCode)) {
-            this.responseData.setResult(new ErrorResponse(errorCode, msg));
-        }
-
-        return this.responseData;
-    }
-
-    public ResponseData<T> setErrorMsg(String msg) {
-        this.responseData.setMessage(msg);
-        this.responseData.setCode(500);
-        this.responseData.setSuccess(false);
-        return this.responseData;
-    }
-
+    /**
+     * 🔓捕获BizException时使用（捕获后直接使用errorCode+message填充返回）
+     *
+     * @param errorCode errorCode
+     * @param msg msg
+     * @return ResponseData
+     */
     public ResponseData<T> setErrorMsg(String errorCode, String msg) {
-        this.responseData.setMessage(msg);
-        this.responseData.setCode(500);
         this.responseData.setSuccess(false);
+        this.responseData.setCode(500);
+        this.responseData.setMessage(msg);
         if (StringUtils.isNotEmpty(errorCode)) {
             this.responseData.setResult(new ErrorResponse(errorCode, msg));
         }
 
-        return this.responseData;
-    }
-
-    public ResponseData<T> setErrorMsg(Integer code, String msg) {
-        this.responseData.setMessage(msg);
-        this.responseData.setCode(null == code ? 500 : code);
-        this.responseData.setSuccess(false);
         return this.responseData;
     }
 
