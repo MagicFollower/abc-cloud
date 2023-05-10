8ecde60f5e4db775b998fa92c1d00a6c82a1e089
YuXiang <webturing256@163.com>
2023-05-10 20:32:17 +0800

    🔓[更新]: abc-common-core添加SpringHelper，用于从Spring IOC容器中获取实例

---
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/helper/SpringHelper.java
@@ -0,0 +1,44 @@
+package com.abc.system.common.helper;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * SpringHelper
+ *
+ * @Description SpringHelper Spring帮助器，区别于Utils，Helper工具会存在于SpringIOC容器
+ * @Author Trivis
+ * @Date 2023/5/10 20:10
+ * @Version 1.0
+ */
+public class SpringHelper implements ApplicationContextAware {
+    private static ApplicationContext APPLICATION_CONTEXT;
+
+    public SpringHelper() {
+    }
+
+    public static <T> T getBean(String beanName) {
+        return APPLICATION_CONTEXT.containsBean(beanName) ? (T) APPLICATION_CONTEXT.getBean(beanName) : null;
+    }
+
+    public static <T> T getBean(Class<T> className) {
+        return APPLICATION_CONTEXT.getBean(className);
+    }
+
+    public static <T> Map<String, T> getBeansByType(Class<T> baseType) {
+        return APPLICATION_CONTEXT.getBeansOfType(baseType);
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return APPLICATION_CONTEXT;
+    }
+
+    public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
+        APPLICATION_CONTEXT = applicationContext;
+    }
+}
