976da1c9e6a2ddf6a36043d8caba6f6ca55936c3
YuXiang <webturing256@163.com>
2023-05-12 21:42:47 +0800

    🔓[更新]: 统一条形码/二维码API 1.名称； 2.写入流+输出字符串。

--- abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-qrcode/src/main/java/com/abc/system/common/qrcode/util/BarCodeUtils.java
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-qrcode/src/main/java/com/abc/system/common/qrcode/util/BarCodeUtils.java
@@ -11,6 +11,8 @@ import com.google.zxing.common.BitMatrix;
 import lombok.extern.slf4j.Slf4j;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Objects;
 
 /**
@@ -29,7 +31,42 @@ public class BarCodeUtils {
     }
 
     /**
-     * 根据code生成相应的一维码
+     * 生成条形码
+     * <pre>
+     *     直接向输出流中写入二维码
+     * </pre>
+     *
+     * @param content 条形码内容
+     * @param width   图片宽度
+     * @param height  图片高度
+     * @param stream  输出流
+     * @throws BizException BizException
+     */
+    public static void createBarCode(String content, int width, int height, OutputStream stream) throws BizException {
+        BitMatrix matrix = null;
+        try {
+            matrix = new MultiFormatWriter().encode(content, BarcodeFormat.CODE_128, width, height, null);
+            MatrixToImageWriter.writeToStream(matrix, ImageTypeEnum.JPEG.getCode(), stream);
+        } catch (Exception e) {
+            log.error(">>>>>>>>>>> create bar code failed|exception: <<<<<<<<<<<", e);
+            throw new BizException(SystemRetCodeConstants.SYSTEM_BUSINESS.getMessage(), "条形码生成失败，请稍候重试");
+        } finally {
+            if (Objects.nonNull(matrix)) {
+                matrix.clear();
+            }
+            if (Objects.nonNull(stream)) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    log.error(">>>>>>>>>>>create QR code|close stream failed|exception: <<<<<<<<<<<", e);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 根据content生成条形码
      *
      * @param content 条形码内容
      * @param width   图片宽度
@@ -37,8 +74,7 @@ public class BarCodeUtils {
      * @return 前端src直接使用的Base64字符串
      * @throws BizException BizException
      */
-    public static String generateCode(String content, int width, int height) throws BizException {
-        //定义位图矩阵BitMatrix
+    public static String createBarCodeString(String content, int width, int height) throws BizException {
         BitMatrix matrix = null;
         try {
             matrix = new MultiFormatWriter().encode(content, BarcodeFormat.CODE_128, width, height, null);
