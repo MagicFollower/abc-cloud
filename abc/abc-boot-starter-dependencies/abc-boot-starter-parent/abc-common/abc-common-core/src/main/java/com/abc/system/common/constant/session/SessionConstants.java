45a9207a59c034f6297b31c5f2abda303f9a9ecf
YuXiang <webturing256@163.com>
2023-05-13 22:07:01 +0800

    🔓[更新]: common-core中新增Session常量（将部分模块常量存放至common-core中是合适的，这将减少模块之间的过度依赖问题）

---
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/constant/session/SessionConstants.java
@@ -0,0 +1,27 @@
+package com.abc.system.common.constant.session;
+
+/**
+ * 用户会话常量定义
+ *
+ * @Description 用户会话常量定义
+ * @Author Trivis
+ * @Date 2023/5/13 22:00
+ * @Version 1.0
+ */
+public interface SessionConstants {
+
+    /**
+     * 会话默认超时时间30分钟
+     */
+    Long DEFAULT_TIME_OUT = 30L;
+
+    /**
+     * 客户端唯一标识
+     */
+    String EUID = "euid";
+    /**
+     * 用户登录会话缓存前缀
+     * 格式：USER_${euid}:会员信息字符串
+     */
+    String USER_ACCOUNT = "USER_%s";
+}
