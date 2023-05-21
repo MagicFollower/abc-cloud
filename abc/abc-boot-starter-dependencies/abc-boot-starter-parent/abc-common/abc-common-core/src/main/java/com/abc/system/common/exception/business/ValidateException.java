9423a9677d9237758bb62a4604b619d9488af654
YuXiang <webturing256@163.com>
2023-05-21 17:15:10 +0800

    🔓[新增]: 校验异常（ValidateException）

---
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/exception/business/ValidateException.java
@@ -0,0 +1,42 @@
+package com.abc.system.common.exception.business;
+
+import com.abc.system.common.constant.SystemRetCodeConstants;
+import com.abc.system.common.exception.base.BaseException;
+
+/**
+ * 校验异常
+ */
+public class ValidateException extends BaseException {
+    public ValidateException() {
+    }
+
+    public ValidateException(SystemRetCodeConstants systemRetCodeConstants) {
+        super();
+        this.errorCode = systemRetCodeConstants.getCode();
+        this.message = systemRetCodeConstants.getMessage();
+    }
+
+    public ValidateException(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ValidateException(Throwable cause) {
+        super(cause);
+    }
+
+    public ValidateException(String errorCode, Throwable cause) {
+        super(cause);
+        this.errorCode = errorCode;
+    }
+
+    public ValidateException(String errorCode, String message) {
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+
+    public ValidateException(String errorCode, String message, Throwable cause) {
+        super(cause);
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+}
