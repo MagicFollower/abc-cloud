0bf2d192b15a69de2ed448146c264f7cfd344e46
YuXiang <webturing256@163.com>
2023-05-31 09:21:14 +0800

    🔓[新增]: ServletUtils#renderString

---
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-web/src/main/java/com/abc/system/web/util/ServletUtils.java
@@ -0,0 +1,46 @@
+package com.abc.system.web.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * ServletUtils
+ *
+ * @Description ServletUtils 详细介绍
+ * @Author Trivis
+ * @Date 2023/5/31 9:13
+ * @Version 1.0
+ */
+@Slf4j
+public class ServletUtils {
+    /**
+     * 将字符串渲染到客户端
+     * <pre>
+     * 1.响应状态码：   200
+     * 2.响应内容类型： application/json
+     * 3.响应内容编码： UTF-8
+     * 🤔️{@code response.getWriter().print(string);}和{@code response.getWriter().write(string);}的区别是什么？
+     *   → print()方法会将给定的字符串写入到响应输出流中，但不会自动刷新输出流，需要手动调用flush()方法或者等待输出流关闭时才会将内容发送给客户端。print()方法可以接受任何类型的参数，并将其转换为字符串输出。
+     *   → write()方法与print()方法类似，也会将给定的字符串写入到响应输出流中。但是，write()方法不仅会将字符串写入到响应输出流中，还会自动刷新输出流，将内容发送给客户端。write()方法只能接受字符串类型的参数。
+     *   因此，如果需要手动控制输出流的刷新时机，可以使用print()方法；如果不需要手动控制输出流的刷新时机，可以使用write()方法，它会自动刷新输出流。
+     * </pre>
+     *
+     * @param response 渲染响应体（HttpServletResponse）
+     * @param string   待渲染的字符串
+     */
+    public static void renderString(HttpServletResponse response, String string) {
+        try {
+            response.setStatus(HttpStatus.OK.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+            response.getWriter().write(string);
+        } catch (IOException e) {
+            log.error(">>>>>>>>|ServletUtils#renderString|exception:{}<<<<<<<<", e.getMessage());
+        }
+    }
+}
