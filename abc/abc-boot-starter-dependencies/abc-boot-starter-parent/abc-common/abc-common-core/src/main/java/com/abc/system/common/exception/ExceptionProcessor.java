d667b6ce94168127ac1ceebee26fe8b4cef60ee1
YuXiang <webturing256@163.com>
2023-07-03 13:55:44 +0800

    [更新]: 新增ExceptionProcessor工具。简化微服务Service层异常包装返回。

---
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/exception/ExceptionProcessor.java
@@ -0,0 +1,45 @@
+package com.abc.system.common.exception;
+
+import com.abc.system.common.constant.SystemRetCodeConstants;
+import com.abc.system.common.exception.base.BaseException;
+import com.abc.system.common.response.AbstractResponse;
+
+/**
+ * 异常处理器工具
+ *
+ * @Description
+ * <pre>
+ *   1.用于微服务Service层对异常（服务层可预测异常BaseException的子类）信息的包装，将异常编码+信息写入到AbstractResponse中；
+ *   2.命名风格和ResponseProcessor保持一致。
+ * </pre>
+ * @Author Trivis
+ * @Date 2023/7/3 13:10
+ * @Version 1.0
+ */
+public class ExceptionProcessor {
+
+    private ExceptionProcessor(){}
+
+    public static AbstractResponse wrapAndHandleException(AbstractResponse abstractResponse, Exception e) {
+        if(e instanceof BaseException){
+            abstractResponse.setCode(((BaseException) e).getErrorCode());
+            abstractResponse.setMsg(e.getMessage());
+        }else{
+            // 未预测异常，不处理
+            abstractResponse.fill(SystemRetCodeConstants.SYSTEM_ERROR);
+        }
+        return abstractResponse;
+    }
+
+    public static AbstractResponse wrapAndHandleException(AbstractResponse abstractResponse, Exception e,
+                                                          String predictableErrorMsg) {
+        if(e instanceof BaseException){
+            abstractResponse.setCode(((BaseException) e).getErrorCode());
+            abstractResponse.setMsg(predictableErrorMsg);
+        }else{
+            // 未预测异常，不处理
+            abstractResponse.fill(SystemRetCodeConstants.SYSTEM_ERROR);
+        }
+        return abstractResponse;
+    }
+}
