72e0960dd7aa17d4bddf8c808ac5b04e3bb16b64
YuXiang <webturing256@163.com>
2023-07-03 13:34:24 +0800

    [更新]: AbstractResponse抽象类新增fill方法的两种重载，支持直接使用SystemRetCodeConstants常量填充AbstractResponse中的code和msg。

--- abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/response/AbstractResponse.java
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/response/AbstractResponse.java
@@ -1,5 +1,6 @@
 package com.abc.system.common.response;
 
+import com.abc.system.common.constant.SystemRetCodeConstants;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,7 @@ import java.io.Serializable;
 
 /**
  * 系统内部Service层响应基类
- *   → 常用在Service层方法正常响应时，手动填充成功编码+成功消息
+ * → 常用在Service层方法正常响应时，手动填充成功编码+成功消息
  * 1.code:String → 命名不同，区别于异常基类中的errorCode
  * 2.msg:String  → 命名不同，区别于异常基类中的message
  */
@@ -18,4 +19,14 @@ import java.io.Serializable;
 public abstract class AbstractResponse implements Serializable {
     protected String code;
     protected String msg;
+
+    public void fill(SystemRetCodeConstants systemRetCodeConstants) {
+        code = systemRetCodeConstants.getCode();
+        msg = systemRetCodeConstants.getMessage();
+    }
+
+    public void fill(SystemRetCodeConstants systemRetCodeConstants, String errorMessage) {
+        code = systemRetCodeConstants.getCode();
+        msg = errorMessage;
+    }
 }
