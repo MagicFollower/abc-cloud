4c7eadcd6f849ac14571b3b3ed8cc1b2b09bf9c3
YuXiang <webturing256@163.com>
2023-07-29 23:27:32 +0800

    🔓[更新]：新增部分注释，关于AbstractRequest#requestCheck

--- abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/request/AbstractRequest.java
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/request/AbstractRequest.java
@@ -22,9 +22,11 @@ public abstract class AbstractRequest implements Serializable {
 
     /**
      * 查询请求参数校验，这是一个tip，它会提醒你要在每一个服务中对参数进行校验
+     * <pre>
      * 1. 你可以为CRUD+Stop都设置单独的校验方法，和这里一样；
-     * 2. 这个方法约定用于查询参数的请求校验。
-     *
+     * 2. 这个方法约定用于查询参数的请求校验；
+     * 3. 将该方法用于Controller层，在进行服务RPC之前校验会有效减少无效服务调用。
+     * </pre>
      * @throws BizException 校验失败时，抛出BizException
      */
     protected abstract void requestCheck() throws BizException;
