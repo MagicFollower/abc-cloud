f05e78e43183c036c92d819a24d76b09a634bc3f
YuXiang <webturing256@163.com>
2023-08-09 20:27:27 +0800

    🔓[更新]：优化tk-mybatis中ID生成器（GenId）的使用文档注释

--- abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-dao/src/main/java/com/abc/system/dao/tkmybatis/idgenerator/AbcTkGlobalIdGen.java
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-dao/src/main/java/com/abc/system/dao/tkmybatis/idgenerator/AbcTkGlobalIdGen.java
@@ -5,6 +5,19 @@ import tk.mybatis.mapper.genid.GenId;
 
 /**
  * AbcTkGlobalIdGen（雪花算法）
+ * <pre>
+ * 🤔️如何使用它？
+ * 1.tk-mybatis在实体层使用javax.persistence的注解：@Table/@Id/@Column
+ *   1.1 你需要将@Id放置在主键上，同时使用@KeySql注解，将实现GenId接口的ID生成类作为genId的属性传入注解，像这样⤵️
+ * {@code
+ *      @Id
+ *      @KeySql(genId = AbcTkGlobalIdGen.class)
+ *      private Long id;
+ * }
+ * 🤔️它什么时候生效？
+ * 1.insert/insertSelective的时候会生效
+ * 2.如果insert/insertSelective的实体中id已经赋值，则使用已经存在的值，当id=null时，会自动生成id并填充
+ * </pre>
  *
  * @Description AbcTkGlobalIdGen TkID自动生成器，使用MybatisPlus的雪花算法生成器
  * @Author Trivis
