e239aed80fbcb22e62c7031634bbf92207cc36f7
YuXiang <webturing256@163.com>
2023-08-19 14:35:04 +0800

    🔓publicPath统一为"/", 但是API会通过utils/conf.js进行控制是否添加/dev前缀 1.router接口的定义与跳转不需要考虑/dev； 2.Axios请求会根据utils/conf.js中的配置自动决定是否添加前缀/dev。

--- abc/abc-ui/src/utils/conf.js
+++ abc/abc-ui/src/utils/conf.js
@@ -1,4 +1,4 @@
-const HOST = '';
+const HOST = process.env.NODE_ENV === 'production' ? '' : '';
 /* 测试环境publicPath设置为/dev，API请求同步变更 */
 const GLOBAL_URL_PREFIX = process.env.NODE_ENV === 'production' ? '' : '/dev';
 
