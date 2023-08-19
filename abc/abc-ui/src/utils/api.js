7459963360f72464df3b737ede0f0c280a8926af
YuXiang <webturing256@163.com>
2023-08-19 14:38:55 +0800

    🔓Axios接口响应验证逻辑更新，统一200状态码，异常类型使用响应中result.errorCode判定 1.关于响应值类型请阅读类：com.abc.system.common.response.ResponseData

--- abc/abc-ui/src/utils/api.js
+++ abc/abc-ui/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import {Message} from 'element-ui';
 import C from './conf';
+import Vue from 'vue';
+const vm = new Vue();
 
 axios.defaults.headers.post['Content-Type'] = 'application/json; charset=UTF-8';
 axios.defaults.withCredentials = true;
@@ -26,20 +28,37 @@ function ajax(url, type, options, config) {
         }
 
         if (!success) {
-          if (data.errorCode === 403) {
+          // → 未登录访问非登录页交互逻辑修改
+          //   → 当前：根据发送请求的结果进行判断跳转，这样会导致存在页面跳转时闪动的问题😟
+          //   → 新版：在页面弹窗登录过期弹窗，弹窗存在一个确认按钮，点击跳转至登录页😊
+          // // 约定: 任何API都将返回200, 状态码通过响应数据中的code+success进行判断
+          // if (data.result.errorCode === '009401') {
+          //   const store = window.localStorage;
+          //   store.removeItem('Access-Token');
+          //   store.removeItem('username');
+          //   location.href = '#/login';
+          //   return;
+          // }
+          if(data.result.errorCode === '009401') {
             const store = window.localStorage;
             store.removeItem('Access-Token');
             store.removeItem('username');
-            location.href = '#/login';
-            return;
+            vm.$confirm('Login status expired, please log in again', 'Prompt', {
+              confirmButtonText: 'confirm',
+              cancelButtonText: 'cancel',
+              type: 'warning'
+            }).then(() => {
+              location.href = '#/login';
+            }).catch(() => {
+              location.href = '#/login';
+            });
+          } else {
+            Message({
+              message: data.message,
+              type: 'error',
+              duration: 2 * 1000
+            });
           }
-          reject(data);
-          Message({
-            message: data.errorMsg,
-            type: 'error',
-            duration: 2 * 1000
-          });
-          return;
         }
       })
       .catch(error => {
