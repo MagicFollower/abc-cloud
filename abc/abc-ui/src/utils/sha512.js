ebecf803e8f88740b524489e71ad9597037c3a77
YuXiang <webturing256@163.com>
2023-08-20 11:29:02 +0800

    🔓eslint code format

--- abc/abc-ui/src/utils/sha512.js
+++ abc/abc-ui/src/utils/sha512.js
@@ -1,8 +1,8 @@
 import CryptoJS from 'crypto-js';
 
 export function sha512Hex(plainText) {
-    return CryptoJS.SHA512(plainText).toString();
+  return CryptoJS.SHA512(plainText).toString();
 }
 export function sha512Base64(plainText) {
-    return CryptoJS.SHA512(plainText).toString(CryptoJS.enc.Base64);
+  return CryptoJS.SHA512(plainText).toString(CryptoJS.enc.Base64);
 }
