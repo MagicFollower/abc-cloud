c3a3f6baea5ecf6f92fcfd1e08486103587ab3a6
YuXiang <webturing256@163.com>
2023-08-21 09:45:16 +0800

    🔓abc-backend业务模块更新！ 1.接入abc-boot-starter-parent作为顶级parent, 降低子模块配置复杂性。

--- abc/abc-backend/abc-backend-service/src/main/java/com/abc/business/web/dal/persistence/AbcProductMapper.java
+++ abc/abc-backend/abc-backend-service/src/main/java/com/abc/business/web/dal/persistence/AbcProductMapper.java
@@ -3,6 +3,5 @@ package com.abc.business.web.dal.persistence;
 import com.abc.business.web.dal.entity.AbcProduct;
 import tk.mybatis.mapper.common.Mapper;
 
-@org.apache.ibatis.annotations.Mapper
 public interface AbcProductMapper extends Mapper<AbcProduct> {
 }
--- abc/abc-backend/abc-backend-service/src/main/java/com/abc/business/web/dal/persistence/AbcProductTypeMapper.java
+++ abc/abc-backend/abc-backend-service/src/main/java/com/abc/business/web/dal/persistence/AbcProductTypeMapper.java
@@ -3,6 +3,5 @@ package com.abc.business.web.dal.persistence;
 import com.abc.business.web.dal.entity.AbcProductType;
 import tk.mybatis.mapper.common.Mapper;
 
-@org.apache.ibatis.annotations.Mapper
 public interface AbcProductTypeMapper extends Mapper<AbcProductType> {
 }
