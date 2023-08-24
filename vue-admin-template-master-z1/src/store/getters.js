0f46aa201f58cc103adefe849127a11da0df04f1
YuXiang <webturing256@163.com>
2023-08-24 08:26:09 +0800

    add some explanation 1.vuex => getters.js

--- vue-admin-template-master-z1/src/store/getters.js
+++ vue-admin-template-master-z1/src/store/getters.js
@@ -1,10 +1,11 @@
 // use getters to get state directly from different modules.
 // 1.vuex => vuex.<moduleAttributeName_in_vuex_modules_attribute>.<attribute_in_state_object>
+// 2.function value has one parameter [state] => you also can use 'this.$store.state.settings.title' in vue-component.
 const getters = {
-  sidebar: vuex => vuex.app.sidebar,
-  device: vuex => vuex.app.device,
-  token: vuex => vuex.user.token,
-  avatar: vuex => vuex.user.avatar,
-  name: vuex => vuex.user.name
+  sidebar: state => state.app.sidebar,
+  device: state => state.app.device,
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name
 }
 export default getters
