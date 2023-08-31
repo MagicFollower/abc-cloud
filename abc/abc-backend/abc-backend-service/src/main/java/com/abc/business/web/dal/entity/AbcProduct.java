9babe7eb3340cc47dfcf39835d584796d1f9387e
MKello <webturing256@163.com>
2023-08-31 21:59:18 +0800

    ✨update two entities with AbcBaseEntity[abc-common-dao]!

--- abc/abc-backend/abc-backend-service/src/main/java/com/abc/business/web/dal/entity/AbcProduct.java
+++ abc/abc-backend/abc-backend-service/src/main/java/com/abc/business/web/dal/entity/AbcProduct.java
@@ -1,27 +1,23 @@
 package com.abc.business.web.dal.entity;
 
-import java.math.BigDecimal;
-import java.util.Date;
-import javax.persistence.*;
-
-import com.abc.system.dao.tkmybatis.idgenerator.AbcTkGlobalIdGen;
+import com.abc.system.dao.base.AbcBaseEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import tk.mybatis.mapper.annotation.KeySql;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.math.BigDecimal;
 
 /**
  * 表名：abc_product
  * 表注释：商品信息
-*/
+ */
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @Table(name = "`abc_product`")
-public class AbcProduct {
-    @Id
-    @Column(name = "`id`")
-    @KeySql(genId = AbcTkGlobalIdGen.class)
-    private Long id;
-
+public class AbcProduct extends AbcBaseEntity {
     /**
      * 类型id
      */
@@ -51,34 +47,4 @@ public class AbcProduct {
      */
     @Column(name = "`sale_price`")
     private BigDecimal salePrice;
-
-    /**
-     * 备注
-     */
-    @Column(name = "`memo`")
-    private String memo;
-
-    /**
-     * 创建时间
-     */
-    @Column(name = "`create_time`")
-    private Date createTime;
-
-    /**
-     * 创建用户（账号）
-     */
-    @Column(name = "`create_user`")
-    private String createUser;
-
-    /**
-     * 更新时间
-     */
-    @Column(name = "`update_time`")
-    private Date updateTime;
-
-    /**
-     * 更新用户（账号）
-     */
-    @Column(name = "`update_user`")
-    private String updateUser;
 }
--- abc/abc-backend/abc-backend-service/src/main/java/com/abc/business/web/dal/entity/AbcProductType.java
+++ abc/abc-backend/abc-backend-service/src/main/java/com/abc/business/web/dal/entity/AbcProductType.java
@@ -1,28 +1,22 @@
 package com.abc.business.web.dal.entity;
 
-import com.abc.system.dao.tkmybatis.idgenerator.AbcTkGlobalIdGen;
+import com.abc.system.dao.base.AbcBaseEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import tk.mybatis.mapper.annotation.KeySql;
 
 import javax.persistence.Column;
-import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Date;
 
 /**
  * 表名：abc_product_type
  * 表注释：商品类型
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @Table(name = "`abc_product_type`")
-public class AbcProductType {
-    @Id
-    @Column(name = "`id`")
-    @KeySql(genId = AbcTkGlobalIdGen.class)
-    private Long id;
-
+public class AbcProductType extends AbcBaseEntity {
     /**
      * 商品类型编码
      */
@@ -34,34 +28,4 @@ public class AbcProductType {
      */
     @Column(name = "`name`")
     private String name;
-
-    /**
-     * 备注
-     */
-    @Column(name = "`memo`")
-    private String memo;
-
-    /**
-     * 创建时间
-     */
-    @Column(name = "`create_time`")
-    private Date createTime;
-
-    /**
-     * 创建用户（账号）
-     */
-    @Column(name = "`create_user`")
-    private String createUser;
-
-    /**
-     * 更新时间
-     */
-    @Column(name = "`update_time`")
-    private Date updateTime;
-
-    /**
-     * 更新用户
-     */
-    @Column(name = "`update_user`")
-    private String updateUser;
 }
