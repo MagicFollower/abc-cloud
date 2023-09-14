76b1ea72c4e04632ee7aab790c8e5ef6b00a89ab
MKello <webturing256@163.com>
2023-09-14 09:38:48 +0800

    ✨Druid连接密码加密配置方式更新（自定义DruidPasswordCallback） 1.现在存在两种方式配置 1.1 [最简配置]第一种默认方式，druid-spring-boot-starter将会自动完成解密操作； 1.2 [可自定义]第二种自定义DruidPasswordCallback的方式，配置逻辑中可以读取druid.connection-properties配置的Properties格式参数，可以在Callback中进行自定义解密操作。

---
+++ abc/abc-backend/abc-backend-service/src/main/java/com/abc/business/web/config/MyDruidPasswordCallback.java
@@ -0,0 +1,36 @@
+package com.abc.business.web.config;
+
+import com.alibaba.druid.filter.config.ConfigTools;
+import com.alibaba.druid.util.DruidPasswordCallback;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Properties;
+
+/**
+ * 自定义Druid密码解密回调类
+ * <pre>
+ * 1.推荐阅读1：http://raye.wang/springboot-druid-zi-ding-yi-jia-mi-shu-ju-ku-mi-ma/
+ * </pre>
+ *
+ * @Description -
+ * @Author -
+ * @Date 2023/9/14 9:00
+ * @Version 1.0
+ */
+@Slf4j
+public class MyDruidPasswordCallback extends DruidPasswordCallback {
+
+    @Override
+    public void setProperties(Properties properties) {
+        String password = properties.getProperty("password");
+        String publicKey = properties.getProperty("config.decrypt.key");
+        String decrypt = "";
+        try {
+            decrypt = ConfigTools.decrypt(publicKey, password);
+        } catch (Exception e) {
+            log.error(">>>>>>>>|MyDruidPasswordCallback|decrypt password error|exception:", e);
+        }
+        super.setPassword(decrypt.toCharArray());
+        super.setProperties(properties);
+    }
+}
