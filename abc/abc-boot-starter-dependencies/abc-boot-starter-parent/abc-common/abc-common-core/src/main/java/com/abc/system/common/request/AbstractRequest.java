08a821e219327bebdb4ac787bc66480078386c05
MKello <webturing256@163.com>
2023-09-25 22:21:17 +0800

    🚀新增initPageInfo方法，在AbstractRequest服务层请求实体中。 1.分页对象在查询接口中使用，推荐在Service层检测/初始化而不是在Controller层，因为Service中的查询接口不仅仅为Controller提供服务，也会对Service提供服务，统一在Service层检测/初始化可以降低Service使用成本。

--- abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/request/AbstractRequest.java
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-core/src/main/java/com/abc/system/common/request/AbstractRequest.java
@@ -35,7 +35,9 @@ public abstract class AbstractRequest<T> implements Serializable {
     protected List<T> saveItems;
     /* ==========场景二：无可编辑列表========== */
     /* 1.新增数据和编辑数据将完全区分开来，需要使用单独的数据集存储，并需要调用单独的add和update接口🍏🍏 */
-    /* 2.该场景下，删除操作将会立刻生效，需提供单独的API，此时只有一种调用API的情况 */
+    /* 2.该场景下，删除操作将会立刻生效，需提供单独的API，此时只有
+
+    一种调用API的情况 */
     protected List<T> addItems;
     protected List<T> updateItems;
 
@@ -43,6 +45,21 @@ public abstract class AbstractRequest<T> implements Serializable {
     // 分页对象
     protected PageInfo pageInfo;
 
+    /**
+     * 分页对象初始化
+     * <pre>
+     * 1.分页对象在查询接口中使用，推荐在Service层检测/初始化而不是在Controller层，
+     *   因为Service中的查询接口不仅仅为Controller提供服务，也会对Service提供服务，统一在Service层检测/初始化可以降低Service使用成本。
+     * </pre>
+     *
+     * @return PageInfo实例
+     */
+    protected PageInfo initPageInfo() {
+        if (pageInfo == null) {
+            pageInfo = new PageInfo();
+        }
+        return pageInfo;
+    }
 
     /**
      * 查询请求参数校验，这是一个tip，它会提醒你要在每一个服务中对参数进行校验
