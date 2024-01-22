63d91f862c8a890c3611f7e57b781cc26ee33390
MKello <webturing256@163.com>
2024-01-22 21:25:16 +0800

    recover Test.java

--- Test.java
+++ Test.java
@@ -1,12 +1,5 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test.main");
-
-        System.out.println("feature-mkello-new");
-
-        // +++
-        System.out.println("Test.main---111");
-        System.out.println("Test.main---222");
-        System.out.println("Test.main---333");
     }
 }
