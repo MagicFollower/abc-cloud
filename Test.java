f2b57f3af34569f1d18fbf4bd54bc29d0e6a7726
MKello <webturing256@163.com>
2024-01-22 20:13:33 +0800

    DEV-1

--- Test.java
+++ Test.java
@@ -1,8 +1,5 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test.main");
-        System.out.println("feature-mkello-new");
-
-        System.out.println("feature-2024年1月22日20:09:18");
     }
 }
