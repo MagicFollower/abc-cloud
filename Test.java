ef93c55ed17fd3e463dc0e95806c3dae0a6659a4
Millennia Kello <89330933+MagicFollower@users.noreply.github.com>
2024-01-22 20:41:31 +0800

    Revert "Feature xx合并到main"

--- Test.java
+++ Test.java
@@ -3,10 +3,5 @@ public class Test {
         System.out.println("Test.main");
 
         System.out.println("feature-mkello-new");
-
-        // +++
-        System.out.println("Test.main---111");
-        System.out.println("Test.main---222");
-        System.out.println("Test.main---333");
     }
 }
