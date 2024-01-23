ad3ad9d02211a36226013762dc272755bc0cd85f
Millennia Kello <89330933+MagicFollower@users.noreply.github.com>
2024-01-23 22:33:00 +0800

    Revert "Feature xx"

--- Test1.java
+++ Test1.java
@@ -1,5 +1,5 @@
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("feature-xx");
+        System.out.println("Test1.main");
     }
 }
--- Test3.java
+++ Test3.java
@@ -1,5 +1,5 @@
 public class Test3 {
     public static void main(String[] args) {
-        System.out.println("feature-xx");
+        System.out.println("Test3.main");
     }
 }
