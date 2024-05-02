a2fcaa63bae2d7dffd1b98d88d94cff766aa80fc
MKello <webturing256@163.com>
2024-05-03 00:07:21 +0800

    dao模块完善AbcTkMapper注释，提供基本API使用注意事项

--- abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-dao/src/main/java/com/abc/system/dao/tkmybatis/dialect/AbcTkMapper.java
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-dao/src/main/java/com/abc/system/dao/tkmybatis/dialect/AbcTkMapper.java
@@ -8,10 +8,17 @@ import tk.mybatis.mapper.common.MySqlMapper;
 /**
  * AbcTkMapper
  * <pre>
- * 1.Mapper
+ * 1.Mapper                        -> 基本CRUD（使用自定义策略or数据库自增，回填） ------ Official
  * 2.MySqlMapper
- *   2.1 insertUseGeneratedKeys    -> 强制使用数据库主键策略（回填）
- *   2.2 insertList                -> 强制使用数据库主键策略（回填）
+ *   2.1 insertUseGeneratedKeys    -> 强制使用数据库主键策略（回填），主键不自增时报错 ------ Official
+ *   2.2 insertList                -> 强制使用数据库主键策略（回填），主键不自增时报错 ------ Official
+ * 3.MySqlBatchInsertMapper
+ *   3.1 insertListSelective       -> 强制使用数据库主键策略（使用数据库主键自增时不回填，主键不自增时报错）
+ *   3.2 batchInsertGenId          -> 使用自定义策略or数据库自增（使用数据库主键自增时不回填，主键不自增时报错）
+ *   3.3 batchInsertGenIdSelective -> 使用自定义策略or数据库自增（使用数据库主键自增时不回填，主键不自增时报错）
+ * 4.MySqlBatchUpdateMapper
+ *   4.1 batchUpdateByPrimaryKey
+ *   4.2 batchUpdateByPrimaryKeySelective
  * </pre>
  * @param <T>
  */
