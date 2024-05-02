33a59c9d61358eab56c3125f9a9fca88b03d4180
MKello <webturing256@163.com>
2024-05-02 23:57:58 +0800

    dao模块注释优化

--- abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-dao/src/main/java/com/abc/system/dao/tkmybatis/dialect/AbcTkMapper.java
+++ abc/abc-boot-starter-dependencies/abc-boot-starter-parent/abc-common/abc-common-dao/src/main/java/com/abc/system/dao/tkmybatis/dialect/AbcTkMapper.java
@@ -5,6 +5,16 @@ import com.abc.system.dao.tkmybatis.dialect.mysql.update.MySqlBatchUpdateMapper;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
+/**
+ * AbcTkMapper
+ * <pre>
+ * 1.Mapper
+ * 2.MySqlMapper
+ *   2.1 insertUseGeneratedKeys    -> 强制使用数据库主键策略（回填）
+ *   2.2 insertList                -> 强制使用数据库主键策略（回填）
+ * </pre>
+ * @param <T>
+ */
 public interface AbcTkMapper<T> extends Mapper<T>, MySqlMapper<T>,
         MySqlBatchInsertMapper<T>, MySqlBatchUpdateMapper<T> {
 }
